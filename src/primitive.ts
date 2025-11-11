ab8030d2985cb7b426907f50a606f2e8cc085cf1
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-11 11:17:12 +0600

    first commit

---
+++ src/primitive.ts
@@ -0,0 +1,15 @@
+//Js-Ts
+//String,Number,Boolean,undefined,null
+//Ts:
+//Unknown,never,void
+
+
+let userName:string='sneara'
+userName=123
+ let userId=123
+ userId=123
+ userName.toFixed()
+ userId.toExponential()
+ 
+ 
+ let isAdmin:boolean=true
